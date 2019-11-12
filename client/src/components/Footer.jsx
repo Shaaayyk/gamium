d3c0a5208516bd14557fa0c3344b0f288e936721
James Markham <jamesmarkham@James-Markhams-MacBook-Pro.local>
2019-11-12 09:22:19 -0500

    some CSS

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-
+    <div id="foot">
+      <footer>
+        <p>&copy; 2019 &mdash; Shay Kelley, Matthew Swaim, James Markham</p>
+      </footer>
     </div>
   )
-}
+    }
