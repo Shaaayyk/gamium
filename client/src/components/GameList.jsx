c62634aa55f171043a0d4b740b99fa719a387939
Matthew Swaim <Mtswaim@Matthews-MacBook-Pro-3.local>
2019-11-14 14:05:11 -0500

    finished game-list

--- client/src/components/GameList.jsx
+++ client/src/components/GameList.jsx
@@ -4,8 +4,23 @@ import { Link } from 'react-router-dom'
 export default function GameList(props) {
   return (
     <div id="game-list">
-      <h1 id='gamium'>Gamium</h1>
-      <p id='mission'>"We at Gamium are dedicated to the production and promotion of indie games and game designers. We strive to provide a platform where designers can share products and get live feedback on those products. Our mission is to provide a platform where we can better ourselves and our projects, together."</p>
+      <div id='hero'>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <div id='title-box'>
+        </div>
+        <div id='mission-box'>
+          <p id='mission'>"We at Gamium are dedicated to the production and promotion of indie games and game designers. We strive to provide a platform where designers can share products and get live feedback on those products. Our mission is to provide a platform where we can better ourselves and our projects, together."</p>
+        </div>
+      </div>
+      <div id='top-box'>
+        <h2 id='top'>Top Games</h2>
+      </div>
       {props.games.map(game => (
         <Link id='game-box' key={game.id} to={`/games/${game.id}`}>
           <div id='game-overlay'>
