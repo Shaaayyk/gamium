3a76ae0c28c71ef30ac17dbc4df714f70b1e95d8
Matthew Swaim <Mtswaim@Matthews-MacBook-Pro-3.local>
2019-11-15 09:30:08 -0500

    updated

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ReviewList(props) {
   return (
     <>
-      {props.reviews &&
+      {props.currentUser &&
         props.reviews.map(review => (
           <div id='review-container'>
             {review.user &&
